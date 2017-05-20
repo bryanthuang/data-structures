b2e1bf41f50f58bfec00ba6a8ee9abf732aea403
Hack Reactor Student <student@hackreactor.com>
2017-05-19 22:26:44 -0700

    Started Binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,26 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
+BinarySearchTree.prototype.insert = function(value){
+  if (value <  this.value) {
+    this.left = value;
+  } else if (value > this.value) {
+    this.right = value;
+  }
+}
+
+BinarySearchTree.prototype.contains = function(value){
+  // return //true/false
+
+}
+
+
+BinarySearchTree.prototype.depthFirstLog = function(callback){
+
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
